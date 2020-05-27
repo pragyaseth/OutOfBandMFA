339de6cbc13eef2c6bfcc9765120553cf59e6631
pragyaseth <pragya.seth@gmail.com>
2020-05-28 03:20:37 +0530

    Update index.js
    updated baseRedirect URL

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Auth from '@okta/okta-vue'
 
 const client = '0oapk7aoaoAK65tJg0h7'
 const orgURL = 'https://seth.oktapreview.com'
-const baseRedirect = 'http://localhost:8080'
+const baseRedirect = 'https://localhost:8080'
 
 Vue.use(Auth, {
   issuer: orgURL + '/oauth2/default',
